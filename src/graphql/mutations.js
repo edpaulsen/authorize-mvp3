ac9b81143018497badaa3cf2db7db127516313be
edpaulsen <ed@paulsenhome.com>
2021-06-10 00:45:25 -0500

    to AWS

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,45 +1,336 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const createTodo = /* GraphQL */ `
-  mutation CreateTodo(
-    $input: CreateTodoInput!
-    $condition: ModelTodoConditionInput
+export const processPayment = /* GraphQL */ `
+  mutation ProcessPayment(
+    $cardInfo: CardInfoInput!
+    $amount: String!
+    $productIds: [String!]!
+    $userId: String!
   ) {
-    createTodo(input: $input, condition: $condition) {
+    processPayment(
+      cardInfo: $cardInfo
+      amount: $amount
+      productIds: $productIds
+      userId: $userId
+    )
+  }
+`;
+export const createUser = /* GraphQL */ `
+  mutation CreateUser(
+    $input: CreateUserInput!
+    $condition: ModelUserConditionInput
+  ) {
+    createUser(input: $input, condition: $condition) {
       id
-      name
-      description
-      createdAt
-      updatedAt
+      email
+      orders {
+        items {
+          id
+          status
+          price
+          userId
+        }
+        nextToken
+      }
     }
   }
 `;
-export const updateTodo = /* GraphQL */ `
-  mutation UpdateTodo(
-    $input: UpdateTodoInput!
-    $condition: ModelTodoConditionInput
+export const updateUser = /* GraphQL */ `
+  mutation UpdateUser(
+    $input: UpdateUserInput!
+    $condition: ModelUserConditionInput
   ) {
-    updateTodo(input: $input, condition: $condition) {
+    updateUser(input: $input, condition: $condition) {
       id
-      name
-      description
-      createdAt
-      updatedAt
+      email
+      orders {
+        items {
+          id
+          status
+          price
+          userId
+        }
+        nextToken
+      }
     }
   }
 `;
-export const deleteTodo = /* GraphQL */ `
-  mutation DeleteTodo(
-    $input: DeleteTodoInput!
-    $condition: ModelTodoConditionInput
+export const deleteUser = /* GraphQL */ `
+  mutation DeleteUser(
+    $input: DeleteUserInput!
+    $condition: ModelUserConditionInput
   ) {
-    deleteTodo(input: $input, condition: $condition) {
+    deleteUser(input: $input, condition: $condition) {
+      id
+      email
+      orders {
+        items {
+          id
+          status
+          price
+          userId
+        }
+        nextToken
+      }
+    }
+  }
+`;
+export const createProducts = /* GraphQL */ `
+  mutation CreateProducts(
+    $input: CreateProductsInput!
+    $condition: ModelProductsConditionInput
+  ) {
+    createProducts(input: $input, condition: $condition) {
       id
       name
+      price
       description
-      createdAt
-      updatedAt
+      images {
+        bucket
+        region
+        key
+        localUri
+        mimeType
+      }
+      orders {
+        items {
+          id
+          productId
+          orderId
+        }
+        nextToken
+      }
+    }
+  }
+`;
+export const updateProducts = /* GraphQL */ `
+  mutation UpdateProducts(
+    $input: UpdateProductsInput!
+    $condition: ModelProductsConditionInput
+  ) {
+    updateProducts(input: $input, condition: $condition) {
+      id
+      name
+      price
+      description
+      images {
+        bucket
+        region
+        key
+        localUri
+        mimeType
+      }
+      orders {
+        items {
+          id
+          productId
+          orderId
+        }
+        nextToken
+      }
+    }
+  }
+`;
+export const deleteProducts = /* GraphQL */ `
+  mutation DeleteProducts(
+    $input: DeleteProductsInput!
+    $condition: ModelProductsConditionInput
+  ) {
+    deleteProducts(input: $input, condition: $condition) {
+      id
+      name
+      price
+      description
+      images {
+        bucket
+        region
+        key
+        localUri
+        mimeType
+      }
+      orders {
+        items {
+          id
+          productId
+          orderId
+        }
+        nextToken
+      }
+    }
+  }
+`;
+export const createOrder = /* GraphQL */ `
+  mutation CreateOrder(
+    $input: CreateOrderInput!
+    $condition: ModelOrderConditionInput
+  ) {
+    createOrder(input: $input, condition: $condition) {
+      id
+      status
+      price
+      userId
+      products {
+        items {
+          id
+          productId
+          orderId
+        }
+        nextToken
+      }
+    }
+  }
+`;
+export const updateOrder = /* GraphQL */ `
+  mutation UpdateOrder(
+    $input: UpdateOrderInput!
+    $condition: ModelOrderConditionInput
+  ) {
+    updateOrder(input: $input, condition: $condition) {
+      id
+      status
+      price
+      userId
+      products {
+        items {
+          id
+          productId
+          orderId
+        }
+        nextToken
+      }
+    }
+  }
+`;
+export const deleteOrder = /* GraphQL */ `
+  mutation DeleteOrder(
+    $input: DeleteOrderInput!
+    $condition: ModelOrderConditionInput
+  ) {
+    deleteOrder(input: $input, condition: $condition) {
+      id
+      status
+      price
+      userId
+      products {
+        items {
+          id
+          productId
+          orderId
+        }
+        nextToken
+      }
+    }
+  }
+`;
+export const createOrderProducts = /* GraphQL */ `
+  mutation CreateOrderProducts(
+    $input: CreateOrderProductsInput!
+    $condition: ModelOrderProductsConditionInput
+  ) {
+    createOrderProducts(input: $input, condition: $condition) {
+      id
+      productId
+      orderId
+      product {
+        id
+        name
+        price
+        description
+        images {
+          bucket
+          region
+          key
+          localUri
+          mimeType
+        }
+        orders {
+          nextToken
+        }
+      }
+      order {
+        id
+        status
+        price
+        userId
+        products {
+          nextToken
+        }
+      }
+    }
+  }
+`;
+export const updateOrderProducts = /* GraphQL */ `
+  mutation UpdateOrderProducts(
+    $input: UpdateOrderProductsInput!
+    $condition: ModelOrderProductsConditionInput
+  ) {
+    updateOrderProducts(input: $input, condition: $condition) {
+      id
+      productId
+      orderId
+      product {
+        id
+        name
+        price
+        description
+        images {
+          bucket
+          region
+          key
+          localUri
+          mimeType
+        }
+        orders {
+          nextToken
+        }
+      }
+      order {
+        id
+        status
+        price
+        userId
+        products {
+          nextToken
+        }
+      }
+    }
+  }
+`;
+export const deleteOrderProducts = /* GraphQL */ `
+  mutation DeleteOrderProducts(
+    $input: DeleteOrderProductsInput!
+    $condition: ModelOrderProductsConditionInput
+  ) {
+    deleteOrderProducts(input: $input, condition: $condition) {
+      id
+      productId
+      orderId
+      product {
+        id
+        name
+        price
+        description
+        images {
+          bucket
+          region
+          key
+          localUri
+          mimeType
+        }
+        orders {
+          nextToken
+        }
+      }
+      order {
+        id
+        status
+        price
+        userId
+        products {
+          nextToken
+        }
+      }
     }
   }
 `;
--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,30 +1,154 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const getTodo = /* GraphQL */ `
-  query GetTodo($id: ID!) {
-    getTodo(id: $id) {
+export const getUser = /* GraphQL */ `
+  query GetUser($id: ID!) {
+    getUser(id: $id) {
       id
-      name
-      description
-      createdAt
-      updatedAt
+      email
+      orders {
+        items {
+          id
+          status
+          price
+          userId
+        }
+        nextToken
+      }
     }
   }
 `;
-export const listTodos = /* GraphQL */ `
-  query ListTodos(
-    $filter: ModelTodoFilterInput
+export const listUsers = /* GraphQL */ `
+  query ListUsers(
+    $filter: ModelUserFilterInput
     $limit: Int
     $nextToken: String
   ) {
-    listTodos(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    listUsers(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        email
+        orders {
+          nextToken
+        }
+      }
+      nextToken
+    }
+  }
+`;
+export const listProductss = /* GraphQL */ `
+  query ListProductss(
+    $filter: ModelProductsFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listProductss(filter: $filter, limit: $limit, nextToken: $nextToken) {
       items {
         id
         name
+        price
         description
-        createdAt
-        updatedAt
+        images {
+          bucket
+          region
+          key
+          localUri
+          mimeType
+        }
+        orders {
+          nextToken
+        }
+      }
+      nextToken
+    }
+  }
+`;
+export const getProducts = /* GraphQL */ `
+  query GetProducts($id: ID!) {
+    getProducts(id: $id) {
+      id
+      name
+      price
+      description
+      images {
+        bucket
+        region
+        key
+        localUri
+        mimeType
+      }
+      orders {
+        items {
+          id
+          productId
+          orderId
+        }
+        nextToken
+      }
+    }
+  }
+`;
+export const listOrders = /* GraphQL */ `
+  query ListOrders(
+    $filter: ModelOrderFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listOrders(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        status
+        price
+        userId
+        products {
+          nextToken
+        }
+      }
+      nextToken
+    }
+  }
+`;
+export const getOrder = /* GraphQL */ `
+  query GetOrder($id: ID!) {
+    getOrder(id: $id) {
+      id
+      status
+      price
+      userId
+      products {
+        items {
+          id
+          productId
+          orderId
+        }
+        nextToken
+      }
+    }
+  }
+`;
+export const orderByUserId = /* GraphQL */ `
+  query OrderByUserId(
+    $userId: ID
+    $sortDirection: ModelSortDirection
+    $filter: ModelOrderFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    orderByUserId(
+      userId: $userId
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        status
+        price
+        userId
+        products {
+          nextToken
+        }
       }
       nextToken
     }
